03c6518e77bf703de2359ac39e7cc696b08b7a7c
Telasja Reynolds <telasjareynolds@gmail.com>
2024-12-17 17:22:48 -0500

    add _id exception

---
+++ .eslintrc.js
@@ -0,0 +1,29 @@
+module.exports = {
+  env: {
+    es2021: true,
+    node: true,
+  },
+
+  // Add the necessary extensions.
+  extends: ["eslint:recommended", "airbnb-base", "prettier"],
+  overrides: [
+    {
+      env: {
+        node: true,
+      },
+      files: [".eslintrc.{js,cjs}"],
+      parserOptions: {
+        sourceType: "script",
+      },
+    },
+  ],
+  parserOptions: {
+    ecmaVersion: "latest",
+    sourceType: "module",
+  },
+  rules: {
+    "no-underscore-dangle": ["error", { allow: ["_id"] }],
+    "no-console": "off",
+    "no-unused-vars": ["error", { argsIgnorePattern: "next" }],
+  },
+};
